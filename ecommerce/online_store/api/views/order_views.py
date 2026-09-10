ab3e78826d4339b2076255024329338866b6b037
Jose-Vilanculo <jozivilanculo@gmail.com>
2026-09-10 13:14:45 +0200

    add email logs

--- ecommerce/online_store/api/views/order_views.py
+++ ecommerce/online_store/api/views/order_views.py
@@ -18,6 +18,11 @@ class OrderViewset(viewsets.ModelViewSet):
     http_method_names = ["get", "post"]
 
     def perform_create(self, serializer):
+        print(settings.EMAIL_HOST)
+        print(settings.EMAIL_PORT)
+        print(settings.EMAIL_USE_TLS)
+        print(settings.EMAIL_HOST_USER)
+        print(settings.DEFAULT_FROM_EMAIL)
         # get cart items
         cart = Cart.objects.get(user=self.request.user)
         print("1 - Got cart")
