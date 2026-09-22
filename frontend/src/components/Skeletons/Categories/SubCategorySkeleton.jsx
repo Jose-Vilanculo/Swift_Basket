bf27c456dd76bc70d7da5765702498665d85b1c6
Jose-Vilanculo <jozivilanculo@gmail.com>
2026-09-22 22:13:21 +0200

    fix: changed the subcategory skeleton loader to a section

--- frontend/src/components/Skeletons/Categories/SubCategorySkeleton.jsx
+++ frontend/src/components/Skeletons/Categories/SubCategorySkeleton.jsx
@@ -4,18 +4,26 @@ import classes from "./SubCategorySkeleton.module.css"
 
 export const SubCategorySkeleton = () => {
     return (
-        <>
-            {[1, 2, 3, 4].map((item) => (
-                <div className={classes.cards} key={item}>
-                <Skeleton className={classes.img} />
-                <div className={classes.text}>
-                    <Skeleton className={classes.name} />
-                    <div className={classes.discover}>
-                        <Skeleton className={classes.discover}/>
+        <section
+            id='sub-category'
+            className={classes["sub-category-section"]}
+        >
+            <div className={classes.container}>
+
+                {[1, 2, 3, 4].map((item) => (
+                    <div className={classes.cards} key={item}>
+                        <Skeleton className={classes.img} />
+                        <div className={classes.text}>
+                            <Skeleton className={classes.name} />
+                            <div className={classes.discover}>
+                                <Skeleton className={classes.discover}/>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ))}
+
             </div>
-            ))}
-        </>
+
+        </section>
     );
 }
